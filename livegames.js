46ec9ecb6889ddfcbbb9cf596cc70f27ae208de6
Cameron Scott <camscottgreen@hotmail.com>
2019-09-25 17:26:23 +1000

    Initial commit

---
+++ livegames.js
@@ -0,0 +1,8 @@
+function Get(yourUrl){
+    var Httpreq = new XMLHttpRequest(); // a new request
+    Httpreq.open("GET",yourUrl,false);
+    Httpreq.send(null);
+    return Httpreq.responseText;          
+}
+
+var json_obj = JSON.parse(Get("https://www.opendota.com/api/live"));
